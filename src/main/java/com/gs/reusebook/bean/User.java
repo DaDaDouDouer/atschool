cf4bbd7e6c1203e7ef72cadfbe4da310eeac6279
qiaojuyang <rache21431@outlook.com>
2017-02-19 10:18:56 +0800

    解决数据库列名和bean属性名不一致的问题 （通过配置mapper的xml）

--- src/main/java/com/gs/reusebook/bean/User.java
+++ src/main/java/com/gs/reusebook/bean/User.java
@@ -1,39 +1,56 @@
 package com.gs.reusebook.bean;
 
+import java.util.Date;
+
 /**
  * 网站一般用户
+ * 
  * @author IceAsh
  *
  */
 public class User {
-	
-	String id;
-	
-	String username;
-	
-	String password;
-	
+
+	private String id;
+
+	private String username;
+
+	private String password;
+
+	private Date createTime;
+
 	public User() {
 	}
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
 }
