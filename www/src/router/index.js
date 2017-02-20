a832d65a53b10455533b4a59f0da3714251e909f
Jack-X-Yang <1487019521@qq.com>
2017-02-20 16:32:50 +0800

    添加vuex和Index组件

--- www/src/router/index.js
+++ www/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Index from 'components/Index'
 import Hello from 'components/Hello'
 
 Vue.use(Router)
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/hello',
       name: 'Hello',
       component: Hello
     }
