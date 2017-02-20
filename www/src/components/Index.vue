c9d4e651a46da2c1b2ba09a5ddc5886cbafa34cc
Jack-X-Yang <1487019521@qq.com>
2017-02-20 16:33:19 +0800

    fixed

---
+++ www/src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    {{ index }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      index: 'index'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
