c73082172c814ff4512b229c0617be97de0316a8
Jack-X-Yang <1487019521@qq.com>
2017-02-24 13:57:03 +0800

    添加注册模块

--- www/src/components/SignUp.vue
+++ www/src/components/SignUp.vue
@@ -1,15 +1,67 @@
 <template>
-  <div class="sign-up">
-    {{ signUp }}
-  </div>
+  <v-card class="sign-up">
+    <v-card-row class="blue darken-1 title">
+      <v-card-title>
+        <span class="white--text">注册</span>
+        <v-spacer></v-spacer>
+      </v-card-title>
+    </v-card-row>
+    <v-card-text class="input" v-for='(input, index) in formItems'>
+      <v-text-input
+        v-model="form[input.name]"
+        :id="input.index"
+        :name="input.name"
+        :type="input.type"
+        :label="input.label"
+        :placeholder="input.placeholder"
+      ></v-text-input>
+    </v-card-text>
+    </v-card-text>
+    <v-card-text>
+      <v-btn primary dark @click.native="submit">注册</v-btn>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'sign-up',
   data () {
     return {
-      signUp: 'sign-up'
+      formItems: [
+        {
+          name: 'username',
+          type: 'text',
+          label: '用户名',
+          placeholder: '请输入您的用户名'
+        },
+        {
+          name: 'password',
+          type: 'password',
+          label: '密码',
+          placeholder: '请输入您的密码'
+        },
+        {
+          name: 'repeatPassword',
+          type: 'password',
+          label: '重复密码',
+          placeholder: '请再次输入您的密码'
+        }
+      ],
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submit () {
+      delete this.form.repeatPassword
+      axios.post('auth/user/register.do', this.form).then(function (response) {
+        console.log(response)
+      })
     }
   }
 }
@@ -17,5 +69,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.sign-up {
+  width: 500px;
+  margin: auto;
+  margin-top: 30px;
+  font-size: 16px;
+  text-align: center;
+}
 
+.title {
+  margin-bottom: 30px;
+}
+
+.input {
+  width: 400px;
+  margin: auto;
+}
 </style>
