d189d0dbed8a469089e2b36a3e064e6c1e676061
Jack-X-Yang <1487019521@qq.com>
2017-03-01 20:22:48 +0800

    添加search

--- www/src/components/Cart.vue
+++ www/src/components/Cart.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="cart">
-    cart
-  </div>
+<div class="cart">
+  <table>
+    <thead>
+      <tr>
+        <th><v-checkbox v-bind:id="'checkbox' + index" filled class="text-xs-center" label="全选"></v-checkbox></th>
+        <th v-for="header in headers" v-text="header"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td v-for="goods in goodsList">
+          <v-checkbox v-bind:id="'checkbox' + index" filled class="text-xs-center"></v-checkbox>
+        </td>
+        <td>{goods}</td>
+        <td>
+          <v-btn icon="icon">
+            <v-icon>edit</v-icon>
+          </v-btn>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </template>
 
 <script>
@@ -9,7 +29,14 @@ export default {
   name: 'cart',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      headers: [
+        '商品信息',
+        '单价',
+        '数量',
+        '金额',
+        '操作'
+      ],
+      goodsList: [1, 2, 3, 4]
     }
   }
 }
---
+++ www/src/components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="search">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
