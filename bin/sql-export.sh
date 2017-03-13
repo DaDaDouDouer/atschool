90d6124a3f3f4fd326d2986b13a0e68f06716703
Jack-X-Yang <1487019521@qq.com>
2017-03-13 13:31:13 +0800

    fixed

--- bin/sql-export.sh
+++ bin/sql-export.sh
@@ -1,6 +1,6 @@
 # 导出数据库
 databaseName="graduate_design"
-sqlPath="../sql/reusebook.sql"
+sqlPath="./sql/reusebook.sql"
 
 rm -rf ${sqlPath};
 mysqldump -u root -p 123456 ${databaseName} > ${sqlPath};
