37e450bd90bdd114c59748efb6a1e137763d45db
Jack-X-Yang <1487019521@qq.com>
2017-03-13 13:53:16 +0800

    使用统一的ajax对象

---
+++ www/src/utils/http.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const config = {
+  // `baseURL` will be prepended to `url` unless `url` is absolute.
+  // It can be convenient to set `baseURL` for an instance of axios to pass relative URLs
+  // to methods of that instance.
+  baseURL: 'http://localhost:8080',
+
+  // `transformRequest` allows changes to the request data before it is sent to the server
+  // This is only applicable for request methods 'PUT', 'POST', and 'PATCH'
+  // The last function in the array must return a string, an ArrayBuffer, or a Stream
+  // transformRequest: [function (data) {
+  //   // Do whatever you want to transform the data
+  //
+  //   return data
+  // }],
+
+  // `transformResponse` allows changes to the response data to be made before
+  // it is passed to then/catch
+  transformResponse: [function (data) {
+    // Do whatever you want to transform the data
+
+    return data
+  }],
+  // `withCredentials` indicates whether or not cross-site Access-Control requests
+  // should be made using credentials
+  withCredentials: false // default
+}
+
+const instance = axios.create(config)
+
+export default instance
