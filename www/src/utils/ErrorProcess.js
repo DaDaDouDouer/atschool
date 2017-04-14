f0be9a3f6a61708b6e749f8daa833e0a7dcd78e9
Jack-X-Yang <1487019521@qq.com>
2017-04-14 10:45:27 +0800

    修改api

--- www/src/utils/ErrorProcess.js
+++ www/src/utils/ErrorProcess.js
@@ -2,19 +2,22 @@ import Router from 'vue-router'
 
 const router = new Router()
 
-export default function (response) {
-  if (typeof response === 'string') {
+export default function (data) {
+  if (typeof data === 'string') {
     try {
-      response = JSON.parse(response)
+      data = JSON.parse(data)
     } catch (e) { /* Ignore */
     }
   }
 
   // 401用户未登录，重定向到登录界面
-  if (response.status === 401) {
+  if (data.status === 401) {
     router.push('login')
     return
   }
 
-  return response.data
+  return {
+    data: data.data,
+    other: data.other
+  }
 }
