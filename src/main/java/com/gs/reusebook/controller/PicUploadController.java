8f006edd795b122923239ed3b38ab34aad26ef29
qiaojuyang <rache21431@outlook.com>
2017-04-20 14:09:25 +0800

    修改登陆验证时的bug；获取图片url全部自动化

--- src/main/java/com/gs/reusebook/controller/PicUploadController.java
+++ src/main/java/com/gs/reusebook/controller/PicUploadController.java
@@ -1,10 +1,12 @@
 package com.gs.reusebook.controller;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,17 +35,46 @@ public class PicUploadController {
 		return picUploadService.upload(file);
 	}
 
-	@RequestMapping(value = "/get/{picId}", method = RequestMethod.GET)
-	public void getPic(@PathVariable("picId") String picId, HttpServletResponse response) {
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public void getPic(@RequestParam(value="picId") String picId, HttpServletResponse response) {
 		
 		picUploadService.get(picId, response);
+	}
+	
+	/**
+	 * 图片url值
+	 */
+	private final static String PIC_URI;
+	
+	/**
+	 *  通过获取注解值组装图片url
+	 */
+	static{
+		// 默认值
+		String classMappingUri = "/file/pic";
+		String methodMappingUri = "/get";
+		String firstParamName = "picId";
+		try {
+		// TODO 校验，存不存在之类的
+			classMappingUri = PicUploadController.class.getAnnotation(RequestMapping.class).value()[0];
+			
+			Method getPicMethod = PicUploadController.class.getMethod("getPic", String.class, HttpServletResponse.class);
+			methodMappingUri = getPicMethod.getAnnotation(RequestMapping.class).value()[0];
+			
+			// 不使用java1.8，不能纯通过反射获取参数名，要分析.class文件
+			firstParamName = ((RequestParam)getPicMethod.getParameterAnnotations()[0][0]).value();
+			
+		} catch (Exception e){
+			// TODO 日志之类的
+		}
+		PIC_URI = classMappingUri + methodMappingUri + ".do?" + firstParamName + "=";
 	}
 	
 	/*
 	 *  得到适用于html img标签的src属性的url值
 	 */
 	static public String getPicUrl(String picId){
-		return "/file/pic/get/" + picId + ".do";
+		return PIC_URI + picId;
 	}
 	
 }
