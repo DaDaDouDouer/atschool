d7a3541872fd15c4e59dda5b93248d9d72666fdb
Jack-X-Yang <1487019521@qq.com>
2017-05-04 17:10:08 +0800

    前端添加个人中心三个子页面：我的订单，更改密码，修改收货地址

---
+++ www/src/components/ManageReceivingAddress.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="manage-receiving-address">
+    manage-receiving-address
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'manage-receiving-address',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ www/src/components/MyOrder.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="my-order">
+    my-order
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-order',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
