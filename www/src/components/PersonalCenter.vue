b54506f525a661ee2c7cb427628d4b8ea662fd5e
Jack-X-Yang <1487019521@qq.com>
2017-05-04 14:59:25 +0800

    前端添加用户中心页面

---
+++ www/src/components/PersonalCenter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="personal-center">
+    personal-center
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'personal-center',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
