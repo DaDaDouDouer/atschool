828d8b4b276142c32788bcc5a62901543e9f78c4
qiaojuyang <rache21431@outlook.com>
2017-05-11 00:11:09 +0800

    Order增加字段，增加创建时间，总价格，用户名，商家名，总价格正确性还未验证，还未增加接口文档

--- src/main/java/com/gs/reusebook/dao/OrderItemDao.java
+++ src/main/java/com/gs/reusebook/dao/OrderItemDao.java
@@ -1,7 +1,12 @@
 package com.gs.reusebook.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.gs.reusebook.bean.OrderItem;
 
 public interface OrderItemDao {
 	public int insertOrderItem(OrderItem orderItem);
+	public OrderItem selectById(String id);
+	public void updateStatus(@Param("status") int status, @Param("id") String id);
+	
 }
