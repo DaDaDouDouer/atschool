e0443bbf21a669225155a7dabf10b4edb6a3f2ff
qiaojuyang <rache21431@outlook.com>
2017-05-22 19:31:28 +0800

    解决用户登录校验过滤器对管理员不管用的问题

--- src/main/java/com/gs/reusebook/bean/Admin.java
+++ src/main/java/com/gs/reusebook/bean/Admin.java
@@ -1,5 +1,7 @@
 package com.gs.reusebook.bean;
 
+import static com.gs.reusebook.util.ReusebookStatic.*;
+
 import com.gs.reusebook.bean.base.AuthBaseBean;
 
 /**
@@ -11,4 +13,28 @@ import com.gs.reusebook.bean.base.AuthBaseBean;
  */
 public class Admin extends AuthBaseBean{
 
+	/**
+	 * 获取管理员用户id的session key
+	 * @return
+	 */
+	@Override
+	public String fatchIdSessionKey(){
+		return ADMIN_ID_SESSION_KEY;
+	}
+	/**
+	 * 获取管理员用户name的session key
+	 * @return
+	 */
+	@Override
+	public String fatchNameSessionKey(){
+		return ADMIN_NAME_SESSION_KEY;
+	}
+	/**
+	 * 获取管理员用户对象的session key
+	 * @return
+	 */
+	@Override
+	public String fatchSessionKey(){
+		return ADMIN_SESSION_KEY;
+	}
 }
