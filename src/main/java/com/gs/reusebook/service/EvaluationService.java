ff4618e866f0ea97ee86cff264ce3cadf94bec9e
qiaojuyang <rache21431@outlook.com>
2017-05-24 01:01:31 +0800

    增加获取全部卖家评论的接口，分页

--- src/main/java/com/gs/reusebook/service/EvaluationService.java
+++ src/main/java/com/gs/reusebook/service/EvaluationService.java
@@ -1,7 +1,9 @@
 package com.gs.reusebook.service;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +14,10 @@ import com.gs.reusebook.bean.Order;
 import com.gs.reusebook.dao.EvaluationDao;
 import com.gs.reusebook.dao.OrderDao;
 import com.gs.reusebook.dao.SellerDao;
+import com.gs.reusebook.paramsbean.CutPageValidatorReturnParams;
 import com.gs.reusebook.paramsbean.ValidatorReturnParams;
 import com.gs.reusebook.util.UiReturn;
+import com.gs.reusebook.validator.CutPageParamsValidator;
 import com.gs.reusebook.validator.GeneralValidator;
 import com.gs.reusebook.validator.base.ValidatorType;
 
@@ -44,6 +48,30 @@ public class EvaluationService {
 		
 		return UiReturn.ok(evaluationDao.selectAll(goodsId), "查询商品评论成功");
 	}
+	
+
+	public UiReturn selectAllBySellerId(String sellerId, Integer pageNo, Integer limit){
+		// 参数校验
+		ValidatorReturnParams result = GeneralValidator.validate(ValidatorType.PKID, sellerId);
+		if(!result.isRight){
+			return UiReturn.notOk(null, result.msg, REQ_ERROR_400);
+		}
+		
+		int goodsAllCount = evaluationDao.selectCountBySellerId(sellerId);
+		
+		// 特殊的分页校验
+		CutPageValidatorReturnParams rst = 
+				CutPageParamsValidator.validate(pageNo, limit, goodsAllCount);
+
+		List<Evaluation> evaluations = evaluationDao.selectAllBySellerId(sellerId, rst.offset, rst.limit); 
+		// 将查询到的总页数放入other中返回
+		Map<String, Integer> otherMap = new HashMap<String, Integer>(1);
+		otherMap.put("pageAllCount", rst.pageAllCount);
+		
+		
+		return UiReturn.ok(evaluations, "查询商品评论成功", otherMap);
+	}
+	
 	
 	/**
 	 * 添加评论，不能对商品重复评论
