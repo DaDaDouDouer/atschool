f02644d54d31a528fdb05317b632f07d94183bc7
qiaojuyang <rache21431@outlook.com>
2017-05-25 09:31:57 +0800

    修正good添加时的sql bug

--- src/test/java/com/gs/reusebook/service/GoodsTest.java
+++ src/test/java/com/gs/reusebook/service/GoodsTest.java
@@ -52,7 +52,7 @@ public class GoodsTest {
 		System.out.println(goodsService.selectAndPagedBySellerId("", "31c0d96d-0e94-4a52-8648-9c39009d5a72", 1, 4));
 	}
 	
-	@Test
+//	@Test
 	public void testGetRealGoods(){
 		UiReturn realGoods = goodsService.getRealGoods("1");
 		System.out.println(realGoods);
@@ -76,8 +76,8 @@ public class GoodsTest {
 		goods.setVia(0);
 		goodsService.addGoods(goods, "17718c86-e8ca-4a9d-96f3-f1a4a3da04ab", "njuptseller");
 	}
-//	@Test
+	@Test
 	public void testAddGoodsByIsbn(){
-		goodsService.addGoodsByIsbn("9787121287220", 12.2, 10, "1", "njuptseller");
+		goodsService.addGoodsByIsbn("9787549220632", 12.2, 10, "31c0d96d-0e94-4a52-8648-9c39009d5a72", "njuptseller");
 	}
 }
