83338d8e726d4c8d425c6fa2abb08ebc776f07d2
Jack-X-Yang <1487019521@qq.com>
2017-06-14 07:59:35 +0000

    前端重新编译

---
+++ www/src/utils/errorProcess.js
@@ -0,0 +1,20 @@
+import Router from 'vue-router'
+
+const router = new Router()
+
+export default function (data) {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (e) { /* Ignore */
+    }
+  }
+
+  // 401用户未登录，重定向到登录界面
+  if (data.status === 401) {
+    router.push('login')
+    return
+  }
+
+  return data
+}
